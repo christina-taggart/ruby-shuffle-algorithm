03ae4a94e7f7639ba0c5944b4a320386c233df17
Apprentice <apprentice@dbc03.local>
2014-01-07 15:51:28 -0800

    Add implementation to shuffler method

--- source/shuffler.rb
+++ source/shuffler.rb
@@ -1,5 +1,25 @@
+# array elements
+# start at the end of the array
+# get a number between zero and the position of the array
+# swap the elements with the end of the array position and the random position
+# do the same with the second to last element until i reach the beginning of array
+
 def shuffle(array)
   # Implement the shuffle method
+
+  idx = array.length-1
+
+  while idx >= 0
+
+    rand_pos = rand(idx+1)
+    temp = array[idx]
+    array[idx] = array[rand_pos]
+    array[rand_pos] = temp
+
+    idx -= 1
+  end
+
+  array
 end
 
 
@@ -10,3 +30,4 @@ sorted_array = (1..10).to_a
 p shuffle(sorted_array)
 p shuffle(sorted_array)
 p shuffle(sorted_array)
+

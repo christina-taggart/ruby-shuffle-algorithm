47f46ab64218c63a8d84bd6a7c0bcba69067bf44
Apprentice <apprentice@dbc26.local>
2014-01-07 16:41:13 -0800

    Added Algorithm challenge

---
+++ shuffle.rb
@@ -0,0 +1,22 @@
+
+
+def shuffle(array)
+  array.sample(array.length)
+end
+
+
+
+
+
+# Driver code:
+sorted_array = (1..10).to_a
+
+# This should print a different sequence of numbers each time
+p shuffle(sorted_array)
+p shuffle(sorted_array)
+p shuffle(sorted_array)
+
+
+
+
+

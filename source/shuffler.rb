0b4bdae0a1b80436e51ff036e5634ce8571e590c
mh120888 <matthew.alan.higgins@gmail.com>
2014-01-12 14:57:27 -0800

    First working version of #shuffle

--- source/shuffler.rb
+++ source/shuffler.rb
@@ -1,12 +1,22 @@
 def shuffle(array)
-  # Implement the shuffle method
+	array_to_randomize = array.clone
+	randomized_array = Array.new
+	array_to_randomize.length.times do
+	  random_number = rand(array_to_randomize.length)
+	  randomized_array << array_to_randomize.delete_at(random_number)
+	end
+	randomized_array
 end
-
 
 # Driver code:
 sorted_array = (1..10).to_a
 
 # This should print a different sequence of numbers each time
-p shuffle(sorted_array)
-p shuffle(sorted_array)
-p shuffle(sorted_array)
+instance_one = 		shuffle(sorted_array)
+instance_two =  	shuffle(sorted_array)
+instance_three =  shuffle(sorted_array)
+
+# THESE SHOULD ALWAYS PRINT FALSE
+p instance_one == instance_two
+p instance_two == instance_three
+p instance_one == instance_three
